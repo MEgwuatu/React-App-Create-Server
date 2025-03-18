d27722bfdf5e25eb0ea00f8e2ccac31b3fed492c
egwuatu modestus <egwuatumodestus@gmail.com>
2025-03-18 10:59:13 +0100

    Updated 18032025 - 10:59

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
+import { Link} from "react-router-dom"
+import './Products.css';
 
 const Products = () => {
   return (
-    <div>
-        <h2 style={{paddingTop: '15vh', color: 'white'}}>This is the Product page.</h2>
+    
+        
       
-    </div>
+        <div class="card-list">
+        <Link to="#" className="card-item">
+            <img src="src/assets/Breaking News Logo.jpeg" alt="Card Image"/>
+            <span class="developer">Developer</span>
+            <h3>A "developer" codes software and websites.</h3>
+            <div class="arrow">
+                <i class="fas fa-arrow-right card-icon"></i>               
+            </div>
+            </Link>
+
+            <Link to="#" className="card-item">
+                <img src="src/assets/Design and Dev picture.webp" alt="Card Image"/>
+                <span class="designer">Desinger</span>
+                <h3>A "designer" is a design expert.</h3>
+                <div class="arrow">
+                    <i class="fas fa-arrow-right card-icon"></i>
+                </div>
+            </Link>
+
+            <Link to="#" className="card-item">
+                <img src="src/assets/Developer Logo.jpeg" alt="Card Image"/>
+                <span class="editor">Editor</span>
+                <h3>An "editor" ensures content quality and accuracy.</h3>
+                <div class="arrow">
+                    <i class="fas fa-arrow-right card-icon"></i>
+                </div>
+            </Link>
+
+        <Link to="#" class="card-item">
+            <img src="src/assets/hero-img.jpg" alt="Card Image"/>
+            <span class="editor">Foreign News</span>
+            <h3>"Foreign News" is very key to be abreast with international changes.</h3>
+            <div class="arrow">
+                <i class="fas fa-arrow-right card-icon"></i>
+            </div>
+
+        </Link>
+
+        <Link to="#" className="card-item">
+            <img src="src/assets/logo.svg" alt="Card Image"/>
+            <span class="editor">Breaking News</span>
+            <h3>"Breaking News" is very important, especially for tourists and investors.</h3>
+            <div class="arrow">
+                <i class="fas fa-arrow-right card-icon"></i>
+            </div>
+
+        </Link>
+
+        <Link to="#" className="card-item">
+            <img src="src/assets/News Flash Logo.jpeg" alt="Card Image"/>
+            <span class="editor">News Flash</span>
+            <h3>"News Flash" is can be very vital in war torn communities.</h3>
+            <div class="arrow">
+                <i class="fas fa-arrow-right card-icon"></i>
+            </div>
+            </Link>
+
+     </div>
+    
   )
 }
 
